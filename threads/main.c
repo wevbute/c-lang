b732285c7fad28866931c8455f43e37af4163fb0
배재환 <baejaehwan@keki.local>
2021-02-28 23:32:00 +0800

    e

---
+++ threads/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *entry_point(void *value) {
+    printf("hello from the second thread");
+    return NULL; 
+}
+
+
+int main(int argc, char const *argv[])
+{
+    pthread_t thread;
+
+    printf("hello from the first thread");
+
+    int num = 23;
+
+    pthread_create(&thread, NULL, entry_point, &num);
+    
+    pthread_join(thread, NULL);
+
+    return EXIT_SUCCESS;
+}
