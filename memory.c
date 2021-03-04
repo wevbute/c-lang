e55d77333f43de8ac17b8895695211b02db212ef
keki <baejaehwan1125@gmail.com>
2021-03-04 15:00:58 +0900

    memory

--- memory.c
+++ memory.c
@@ -3,19 +3,21 @@
 
 int main(int argc, char const *argv[])
 {
-    int e = 5;
-    int* ptr = (int *)malloc(sizeof(int));
+    int* ptr = (int *)malloc(sizeof(int) * 5); // 4바이트 5칸 할당
+    int* ptr2 = (int *)calloc(5, sizeof(int)); // 4바이트 5칸 할당 그리고 메모리의 값 다 0으로 초기화
 
-    if (ptr == NULL) {
+    if (ptr == NULL || ptr2 == NULL) {
         printf("Cannot allocate memory");
         return 1;
     }
 
-    *ptr = e;
+    // *ptr = e; 
 
-    printf("%d\n", *ptr);
+    // printf("%d\n", *ptr); 
+    printf("%d\n", *ptr2);
     
     free(ptr);
+    free(ptr2);
     
     return 0;
 }
